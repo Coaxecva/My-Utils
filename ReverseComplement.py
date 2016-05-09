1925c955cac685738d6e1e321835ee16af1fd625
Quang Tran <tmquang@gmail.com>
2016-05-09 09:22:56 -0500

    Update ReverseComplement.py

--- ReverseComplement.py
+++ ReverseComplement.py
@@ -1,3 +1,26 @@
+class FastaSeq:
+def __init__(self, name, sequence):
+    self.name = name
+    self.sequence = sequence
+
+def get_seqs(file):
+    items = []
+    index = 0
+    for line in file:
+        if line.startswith(">"):
+            if index >= 1:
+                items.append(aninstance)
+            index+=1
+            name = line[:-1]
+            seq = ''
+            aninstance = FastaSeq(name, seq)
+        else:
+            seq += line[:-1]
+            aninstance = FastaSeq(name, seq)
+    items.append(aninstance)
+
+    return items
+
 def ReverseComplement3(seq):
     for base in seq:
         if base not in 'ATCGatcg':
@@ -6,3 +29,10 @@ def ReverseComplement3(seq):
     seq1 = 'ATCGTAGCatcgtagc'
     seq_dict = { seq1[i]:seq1[i+4] for i in range(16) if i < 4 or 8<=i<12 }
     return "".join([seq_dict[base] for base in reversed(seq)])
+
+def main():
+
+	print(ReverseComplement3(seq))
+
+ if __name__ = "main":
+ 	main()
