cec45f474418dfb35e44604366c348379eec33c1
Quang Tran <tmquang@gmail.com>
2016-05-09 09:09:10 -0500

    Add files via upload

---
+++ ReverseComplement.py
@@ -0,0 +1,8 @@
+def ReverseComplement3(seq):
+    for base in seq:
+        if base not in 'ATCGatcg':
+            print "Error: NOT a DNA sequence"
+            return None
+    seq1 = 'ATCGTAGCatcgtagc'
+    seq_dict = { seq1[i]:seq1[i+4] for i in range(16) if i < 4 or 8<=i<12 }
+    return "".join([seq_dict[base] for base in reversed(seq)])
