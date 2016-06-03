6e2abbcecb359983cc103d4b83b9966df3eb530e
Quang Tran <tmquang@gmail.com>
2016-06-03 14:11:21 -0500

    Update SRalignment_runner.py

--- SRalignment_runner.py
+++ SRalignment_runner.py
@@ -4,9 +4,10 @@ ref_path = "~/Indel_Analysis/"
 
 SRmapper_path = "~/quang/SRmapper0.1.5/align "
 SHRiMp_path = "~/quang/SHRiMP_2_2_3/bin/gmapper-ls  --ignore-qvs -o 1 --strata "
-
+Bowtie2_path = "~/quang/bowtie2-2.0.2/bowtie2-align -x "
 
 if __name__ == '__main__':
+	
 	f = open(sys.argv[1])
 	while True:
 		read = f.readline()
@@ -16,7 +17,9 @@ if __name__ == '__main__':
 		print(SRmapper_path+ref_path+"GRCh37_"+read[:-4]+"-SRmapper { "+read[:-3]+"fastq } "+read[:-4]+".SRmapper.sam &")
 		#os.system(SRmapper_path+ref_path+"GRCh37_"+read[:-4]+"-SRmapper { "+read[:-3]+"fastq } "+read[:-4]+".SRmapper.sam &")
 		
-		print(SRmapper_path+ref_path+"GRCh37_"+read[:-4]+"-SRmapper { "+read[:-3]+"fastq } "+read[:-4]+".SRmapper.sam &")
-		#os.system(SRmapper_path+ref_path+"GRCh37_"+read[:-4]+"-SRmapper { "+read[:-3]+"fastq } "+read[:-4]+".SRmapper.sam &")
+		print(SHRiMp_path+read.strip()+" "+ref_path+"GRCh37_"+read[:-4]+".fasta > "+read[:-4]+".SHRiMP.sam &")
+		#os.system(SHRiMp_path+read.strip()+" "+ref_path+"GRCh37_"+read[:-4]+".fasta > "+read[:-4]+".SHRiMP.sam &")
 
-		~/quang/SHRiMP_2_2_3/bin/gmapper-ls  --ignore-qvs -o 1 --strata chr1.fq GRCh37_chr1.fasta > chr1.shrimp.sam &
+		print(Bowtie2_path+ref_path+"GRCh37_"+read[:-4]+"-bowtie2 "+read.strip()+" -S "+read[:-4]+".Bowtie2.sam &")
+		#os.system(Bowtie2_path+ref_path+"GRCh37_"+read[:-4]+"-bowtie2 "+read+" -S "+read[:-4]+".Bowtie2.sam &")
+		
