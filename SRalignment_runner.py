f8d80fc2654b92c67041c3e8b1f353854a08617a
Quang Tran <tmquang@gmail.com>
2016-06-04 16:39:08 -0500

    Update SRalignment_runner.py

--- SRalignment_runner.py
+++ SRalignment_runner.py
@@ -23,23 +23,23 @@ if __name__ == '__main__':
 		#os.system(SRmapper_path+ref_path+"GRCh37_"+read[:-4]+"-SRmapper { "+read[:-3]+"fastq } "+read[:-4]+".SRmapper.sam &")
 		
 		print(SHRiMp_path+read.strip()+" "+ref_path+"GRCh37_"+read[:-4]+".fasta > "+read[:-4]+".SHRiMP.sam")
-		os.system(SHRiMp_path+read.strip()+" "+ref_path+"GRCh37_"+read[:-4]+".fasta > "+read[:-4]+".SHRiMP.sam")
+		os.system(SHRiMp_path+read.strip()+" "+ref_path+"GRCh37_"+read[:-4]+".fasta > "+read[:-4]+".SHRiMP.sam &")
 
 		print(Bowtie2_path+ref_path+"GRCh37_"+read[:-4]+"-bowtie2 "+read.strip()+" -S "+read[:-4]+".Bowtie2.sam")
-		os.system(Bowtie2_path+ref_path+"GRCh37_"+read[:-4]+"-bowtie2 "+read+" -S "+read[:-4]+".Bowtie2.sam")
+		os.system(Bowtie2_path+ref_path+"GRCh37_"+read[:-4]+"-bowtie2 "+read+" -S "+read[:-4]+".Bowtie2.sam &")
 		
 		print(Cushaw2_path+ref_path+"GRCh37_"+read[:-4]+".fasta -f "+read.strip()+" -o "+read[:-4]+".Cushaw2.sam")
-		os.system(Cushaw2_path+ref_path+"GRCh37_"+read[:-4]+".fasta -f "+read.strip()+" -o "+read[:-4]+".Cushaw2.sam")
+		os.system(Cushaw2_path+ref_path+"GRCh37_"+read[:-4]+".fasta -f "+read.strip()+" -o "+read[:-4]+".Cushaw2.sam &")
 		
 		print(Gassst_path+ref_path+"GRCh37_"+read[:-4]+".fasta -i "+read[:-4]+".fa -o "+read[:-4]+".Gassst -p 90 -h 1 && ~/quang/Gassst_v1.28/gassst_to_sam "+read[:-4]+".Gassst "+read[:-4]+".Gassst.sam")
-		os.system(Gassst_path+ref_path+"GRCh37_"+read[:-4]+".fasta -i "+read[:-4]+"fa -o "+read[:-4]+".Gassst -p 90 -h 1 && ~/quang/Gassst_v1.28/gassst_to_sam "+read[:-4]+".Gassst "+read[:-4]+".Gassst.sam")
+		os.system(Gassst_path+ref_path+"GRCh37_"+read[:-4]+".fasta -i "+read[:-4]+"fa -o "+read[:-4]+".Gassst -p 90 -h 1 && ~/quang/Gassst_v1.28/gassst_to_sam "+read[:-4]+".Gassst "+read[:-4]+".Gassst.sam &")
 		
 		print(Razers3_path+read[:-4]+".Razers3.sam "+ref_path+"GRCh37_"+read[:-4]+".fasta "+read.strip())
-		os.system(Razers3_path+read[:-4]+".Razers3.sam "+ref_path+"GRCh37_"+read[:-4]+".fasta "+read.strip())
+		os.system(Razers3_path+read[:-4]+".Razers3.sam "+ref_path+"GRCh37_"+read[:-4]+".fasta "+read.strip()+" &")
 
 		print(BWASW_path+ref_path+"GRCh37_"+read[:-4]+".fasta "+read.strip()+" > "+read[:-4]+".Bwasw.sam")
-		os.system(BWASW_path+ref_path+"GRCh37_"+read[:-4]+".fasta "+read.strip()+" > "+read[:-4]+".Bwasw.sam")
+		os.system(BWASW_path+ref_path+"GRCh37_"+read[:-4]+".fasta "+read.strip()+" > "+read[:-4]+".Bwasw.sam &")
 
 		print(Smalt_path+read[:-4]+".Smalt.sam " +ref_path+"GRCh37_"+read[:-4]+".smalt "+read.strip())
-		os.system(Smalt_path+read[:-4]+".Smalt.sam " +ref_path+"GRCh37_"+read[:-4]+".smalt "+read.strip())
+		os.system(Smalt_path+read[:-4]+".Smalt.sam " +ref_path+"GRCh37_"+read[:-4]+".smalt "+read.strip()+" &")
 		
